07d79638d1f69fa3ff0c38a16da94ca5bf0d3375
dorayakikun <amakunai.dorayaki@gmail.com>
2017-06-21 00:10:49 +0900

    :art: Update mock

--- src/index.js
+++ src/index.js
@@ -1,21 +1,43 @@
+// @flow
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Statistic } from 'semantic-ui-react'
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import { Map, Marker, Popup, Rectangle, TileLayer } from 'react-leaflet'
 
-const position = [35.6638, 139.71805]
+const position1 = [35.66666666666667, 139.5]
+const bounds1 = [[36, 139], [35.333333333333336, 140]]
+
+const position2 = [35.625, 139.6875]
+const bounds2 = [[35.66666666666667, 139.625], [35.583333333333336, 139.75]]
+
+const position3 = [35.6625, 139.71875]
+const bounds3 = [[[35.666666666666664, 139.7125], [35.65833333333333, 139.725]]]
 
 ReactDOM.render(
   <div>
     <Statistic color="teal" value="Hello, world!" label="teal" />
-    <Map center={position} zoom={13}>
+    <Map center={position1} zoom={13}>
       <TileLayer
         url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
         attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
       />
-      <Marker position={position}>
+      <Rectangle bounds={bounds1} />
+      <Marker position={position1}>
         <Popup>
-          <span>A pretty CSS3 popup.<br />Easily customizable.</span>
+          <span>First Mesh</span>
+        </Popup>
+      </Marker>
+      <Rectangle bounds={bounds2} color="#00ff00" />
+      <Marker position={position2}>
+        <Popup>
+          <span>Second Mesh</span>
+        </Popup>
+      </Marker>
+      <Rectangle bounds={bounds3} color="#ff0000" />
+      <Marker position={position3}>
+        <Popup>
+          <span>Third Mesh</span>
         </Popup>
       </Marker>
     </Map>
