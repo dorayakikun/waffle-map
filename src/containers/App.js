c156cc1c3ed683eafefc18244f0863e88d031966
dorayakikun <amakunai.dorayaki@gmail.com>
2017-07-02 00:10:27 +0900

    :art: Refactor the appearance for Top Page

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,26 +1,26 @@
 // @flow
 
 import React from 'react'
-import { Grid, Header } from 'semantic-ui-react'
+import { Container, Grid, Menu, Segment, Sidebar } from 'semantic-ui-react'
 
 import MeshContainer from './MeshContainer'
 import MapContainer from './MapContainer'
 
 const App = () =>
-  <div>
-    <Header as="h1" color="teal" block>
-      Waffle Map
-    </Header>
-    <Grid celled="internally">
-      <Grid.Row>
-        <Grid.Column width={4}>
+  <div style={{ height: '100%' }}>
+    <Sidebar.Pushable as={Segment}>
+      <Sidebar as={Menu} width="wide" visible icon="labeled" vertical inverted>
+        <Menu.Item name="waffleMap">
+          Waffle Map
+        </Menu.Item>
+        <Menu.Item name="meshInput">
           <MeshContainer />
-        </Grid.Column>
-        <Grid.Column width={12}>
-          <MapContainer />
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
+        </Menu.Item>
+      </Sidebar>
+      <Sidebar.Pusher>
+        <MapContainer />
+      </Sidebar.Pusher>
+    </Sidebar.Pushable>
   </div>
 
 export default App
