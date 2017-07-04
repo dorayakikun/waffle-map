a37637d91cfde61ba7bd041cdb76c243481ba3ee
dorayakikun <amakunai.dorayaki@gmail.com>
2017-07-04 21:54:28 +0900

    :green_heart: Add tests for reducers

---
+++ test/reducers/index.test.js
@@ -0,0 +1,39 @@
+// @flow
+
+import * as AppActions from '../../src/actions/AppActions'
+import reducer from '../../src/reducers'
+import * as MeshCalculator from '../../src/meshCalculator'
+
+test('Should handle INPUT_MESHES', () => {
+  const meshesString = '5339'
+  const expectedState = {
+    meshInput: {
+      meshesString,
+      separator: '.'
+    },
+    meshes: [
+      {
+        code: meshesString,
+        center: MeshCalculator.meshToLatLon(meshesString),
+        bounds: MeshCalculator.meshToBounds(meshesString)
+      }
+    ]
+  }
+  expect(reducer(undefined, AppActions.inputMeshes(meshesString))).toEqual(
+    expectedState
+  )
+})
+
+test('Should handle SELECT_SEPARATOR', () => {
+  const separator = ','
+  const expectedState = {
+    meshInput: {
+      meshesString: '',
+      separator
+    },
+    meshes: []
+  }
+  expect(reducer(undefined, AppActions.selectSeparator(separator))).toEqual(
+    expectedState
+  )
+})
