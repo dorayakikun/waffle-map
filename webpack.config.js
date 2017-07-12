054130fe6419433a6642e423f4aefc96ff0b5196
dorayakikun <amakunai.dorayaki@gmail.com>
2017-07-13 01:14:36 +0900

    :art: Migrate to webpack

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+/* eslint-disable */
+
+const path = require('path')
+const webpack = require('webpack')
+
+const BASE_PLUGINS = [
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  })
+]
+
+console.log('env >', process.env.NODE_ENV)
+module.exports = {
+  entry: process.env.NODE_ENV === 'production'
+    ? ['./src/index.js']
+    : [
+      'webpack-dev-server/client?http://localhost:9000',
+      'webpack/hot/only-dev-server',
+      './src/index.js'
+    ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/'
+  },
+  devServer: {
+    contentBase: 'public/',
+    historyApiFallback: true,
+    port: 9000,
+    hot: true
+  },
+  plugins: process.env.NODE_ENV === 'production'
+    ? BASE_PLUGINS.concat(
+      [
+        new webpack.optimize.UglifyJsPlugin({
+          minimize: true,
+          sourceMap: false,
+          compressor: {
+            warnings: false
+          },
+          output: {
+            comments: false
+          }
+        })
+      ]
+    )
+    : BASE_PLUGINS.concat([
+      new webpack.NamedModulesPlugin(),
+      new webpack.NoEmitOnErrorsPlugin(),
+      new webpack.HotModuleReplacementPlugin()
+    ]),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
