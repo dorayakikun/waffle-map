32bfa036dcea77cd553fdf37a2a379fe55ea2888
dorayakikun <amakunai.dorayaki@gmail.com>
2017-08-23 16:52:38 +0900

    :art: Review MarkerInput props

--- src/components/MarkerInput.js
+++ src/components/MarkerInput.js
@@ -1,56 +1,86 @@
 // @flow
 import React, { Component } from 'react'
-import { Button, Dropdown, Icon, Input } from 'semantic-ui-react'
+import { Button, Dropdown, Label, Icon, Input } from 'semantic-ui-react'
+
+export type MarkerInputState = {
+  latLng: string,
+  datum: string
+}
 
 export type MarkerInputProps = {
   latLng: string,
   datum: string,
-  errorMessage: string
+  errorMessage: string,
+  putMarker: (event: Event, state: MarkerInputState) => void,
+  removeAllMarkers: (event: Event, state: MarkerInputState) => void
 }
 
 class MarkerInput extends Component {
   state = {
-    latLng: '',
-    datum: 'degree'
+    latLng: this.props.latLng || '',
+    datum: this.props.datum || 'degree'
   }
 
-  handleChange = (e, { latLng, value }) => {
-    console.log(latLng, value)
-    this.setState({ [latLng]: value })
+  onChangedLatLng = (e: Event, data: { value: string }) =>
+    this.setState({ latLng: data.value })
+
+  onChangedDatum = (e: Event, data: { value: string }) =>
+    this.setState({ datum: data.value })
+
+  handleClickPutAMarker = (event: Event) => {
+    this.props.putMarker(event, this.state)
   }
 
-  handleSubmit = () => {}
+  handleClickRemoveAllMarkers = (event: Event) => {
+    this.props.removeAllMarkers(event, this.state)
+  }
 
   render() {
     const { latLng, datum } = this.state
+    const { putMarker, removeAllMarkers } = this.props
 
     return (
       <div>
         <Input
+          fluid
           inverted
-          label="LatLng"
-          onChange={this.handleChange}
+          label={<Label color="teal">LatLng</Label>}
+          onChange={this.onChangedLatLng}
           placeholder="lat,lng"
+          style={{ marginTop: '10px', marginBottom: '10px' }}
           value={latLng}
         />
-        <Button>Put a marker</Button>
-        <Button>Remove all markers</Button>
-        <div>
-          <Dropdown
-            options={[
-              {
-                text: 'millisec',
-                value: 'millisec'
-              },
-              {
-                text: 'degree',
-                value: 'degree'
-              }
-            ]}
-            text="millisec"
-            value="millisec"
-          />
-        </div>
+        <Dropdown
+          fluid
+          onChange={this.onChangedDatum}
+          options={[
+            {
+              text: 'millisec',
+              value: 'millisec'
+            },
+            {
+              text: 'degree',
+              value: 'degree'
+            }
+          ]}
+          style={{ marginTop: '10px', marginBottom: '10px' }}
+          text={datum}
+          value={datum}
+        />
+        <Button
+          fluid
+          style={{ marginTop: '10px', marginBottom: '10px' }}
+          onClick={this.handleClickPutAMarker}
+        >
+          Put a marker
+        </Button>
+        <Button
+          fluid
+          style={{ marginTop: '10px', marginBottom: '10px' }}
+          onClick={this.handleClickRemoveAllMarkers}
+        >
+          Remove all markers
+        </Button>
       </div>
     )
   }
