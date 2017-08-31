6f293d2d021de7a7d76db9c37f2f71fa9831cdff
dorayakikun <amakunai.dorayaki@gmail.com>
2017-08-31 21:33:58 +0900

    :sparkles: Make enable to put a marker when onEnter

--- src/components/MarkerInput.js
+++ src/components/MarkerInput.js
@@ -30,8 +30,9 @@ class MarkerInput extends Component {
     errorMessage: this.props.errorMessage || ''
   }
 
-  onChangedLatLng = (e: Event, data: { value: string }) =>
+  onChangedLatLng = (e: Event, data: { value: string }) => {
     this.setState({ latLng: data.value })
+  }
 
   onChangedUnit = (e: Event, data: { value: string }) =>
     this.setState({ unit: data.value })
@@ -49,7 +50,13 @@ class MarkerInput extends Component {
     const { putMarker, removeAllMarkers } = this.props
 
     return (
-      <div>
+      <div
+        onKeyPress={(event: Event) => {
+          if (event.key === 'Enter') {
+            this.handleClickPutAMarker(event)
+          }
+        }}
+      >
         <Label color="teal" tag>
           Marker
         </Label>
