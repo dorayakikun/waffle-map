aceba907b0f28e2b4619f212739be570d07d2126
dorayakikun <amakunai.dorayaki@gmail.com>
2017-10-24 23:49:56 +0900

    :art: Change to accordion menu

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React from 'react'
-import { Grid, Image, Menu, Radio, Sidebar } from 'semantic-ui-react'
+import { Accordion, Grid, Image, Menu, Radio, Sidebar } from 'semantic-ui-react'
 
 import MarkerInputContainer from './MarkerInputContainer'
 import MeshInputContainer from './MeshInputContainer'
@@ -12,23 +12,32 @@ import TileToggleContainer from './TileToggleContainer'
 const App = () => (
   <div style={{ height: '100%' }}>
     <Sidebar.Pushable>
-      <Sidebar as={Menu} inverted vertical visible width="wide">
-        <Menu.Item name="waffleMap">
-          <Image src="./images/logo.png" size="mini" spaced />
-          <strong>Waffle Map</strong>
-        </Menu.Item>
-        <Menu.Item name="tileToggle">
-          <TileToggleContainer />
-        </Menu.Item>
-        <Menu.Item name="meshInput">
-          <MarkerInputContainer />
-        </Menu.Item>
-        <Menu.Item name="meshInput">
-          <MeshInputContainer />
-        </Menu.Item>
-        <Menu.Item name="meshDetails">
-          <MeshDetailsContainer />
-        </Menu.Item>
+      <Sidebar inverted visible width="wide">
+        <Accordion as={Menu} fluid inverted vertical>
+          <Menu.Item name="waffleMap">
+            <Image src="./images/logo.png" size="mini" spaced />
+            <strong>Waffle Map</strong>
+          </Menu.Item>
+
+          <Accordion.Title index={0}>
+            Tile Grid
+          </Accordion.Title>
+          <Accordion.Content>
+            <Menu.Item name="tileToggle">
+              <TileToggleContainer />
+            </Menu.Item>
+          </Accordion.Content>
+
+          <Menu.Item name="meshInput">
+            <MarkerInputContainer />
+          </Menu.Item>
+          <Menu.Item name="meshInput">
+            <MeshInputContainer />
+          </Menu.Item>
+          <Menu.Item name="meshDetails">
+            <MeshDetailsContainer />
+          </Menu.Item>
+        </Accordion>
       </Sidebar>
       <Sidebar.Pusher>
         <MapContainer />
