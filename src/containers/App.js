b00729e33e24aa366135827f2de4c1211d8f0baf
dorayakikun <amakunai.dorayaki@gmail.com>
2017-10-31 21:40:47 +0900

    :sparkles: Enable opening and closing of accordion

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React from 'react'
+import React, { Component } from 'react'
 import { Accordion, Grid, Image, Menu, Radio, Sidebar } from 'semantic-ui-react'
 
 import MarkerInputContainer from './MarkerInputContainer'
@@ -9,41 +9,84 @@ import MeshDetailsContainer from './MeshDetailsContainer'
 import MapContainer from './MapContainer'
 import TileToggleContainer from './TileToggleContainer'
 
-const App = () => (
-  <div style={{ height: '100%' }}>
-    <Sidebar.Pushable>
-      <Sidebar inverted visible width="wide">
-        <Accordion as={Menu} fluid inverted vertical>
-          <Menu.Item name="waffleMap">
-            <Image src="./images/logo.png" size="mini" spaced />
-            <strong>Waffle Map</strong>
-          </Menu.Item>
+export default class AccordionExampleMenu extends Component {
+  state = { activeIndex: 2 }
 
-          <Accordion.Title index={0}>
-            Tile Grid
-          </Accordion.Title>
-          <Accordion.Content>
-            <Menu.Item name="tileToggle">
-              <TileToggleContainer />
-            </Menu.Item>
-          </Accordion.Content>
+  handleClick = (e, titleProps) => {
+    const { index } = titleProps
+    const { activeIndex } = this.state
+    const newIndex = activeIndex === index ? -1 : index
 
-          <Menu.Item name="meshInput">
-            <MarkerInputContainer />
-          </Menu.Item>
-          <Menu.Item name="meshInput">
-            <MeshInputContainer />
-          </Menu.Item>
-          <Menu.Item name="meshDetails">
-            <MeshDetailsContainer />
-          </Menu.Item>
-        </Accordion>
-      </Sidebar>
-      <Sidebar.Pusher>
-        <MapContainer />
-      </Sidebar.Pusher>
-    </Sidebar.Pushable>
-  </div>
-)
+    this.setState({ activeIndex: newIndex })
+  }
 
-export default App
+  render() {
+    const { activeIndex } = this.state
+    return (
+      <div style={{ height: '100%' }}>
+        <Sidebar.Pushable>
+          <Sidebar visible width="wide">
+            <Accordion
+              as={Menu}
+              fluid
+              inverted
+              vertical
+              style={{ height: '100%' }}
+            >
+              <Menu.Item name="waffleMap">
+                <Image src="./images/logo.png" size="mini" spaced />
+                <strong>Waffle Map</strong>
+              </Menu.Item>
+
+              <Accordion.Title
+                active={activeIndex === 0}
+                index={0}
+                onClick={this.handleClick}
+              >
+                Tile Grid
+              </Accordion.Title>
+              <Accordion.Content active={activeIndex === 0}>
+                <Menu.Item name="tileToggle">
+                  <TileToggleContainer />
+                </Menu.Item>
+              </Accordion.Content>
+
+              <Accordion.Title
+                active={activeIndex === 1}
+                index={1}
+                onClick={this.handleClick}
+              >
+                Marker
+              </Accordion.Title>
+              <Accordion.Content active={activeIndex === 1}>
+                <Menu.Item name="markerInput">
+                  <MarkerInputContainer />
+                </Menu.Item>
+              </Accordion.Content>
+
+              <Accordion.Title
+                active={activeIndex === 2}
+                index={2}
+                onClick={this.handleClick}
+              >
+                Mesh Code
+              </Accordion.Title>
+
+              <Accordion.Content active={activeIndex === 2}>
+                <Menu.Item name="meshInput">
+                  <MeshInputContainer />
+                </Menu.Item>
+                <Menu.Item name="meshDetails">
+                  <MeshDetailsContainer />
+                </Menu.Item>
+              </Accordion.Content>
+            </Accordion>
+          </Sidebar>
+          <Sidebar.Pusher>
+            <MapContainer />
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </div>
+    )
+  }
+}
