7e2be258e4f309de0edf8b4911c96f406bae3f11
dorayakikun <amakunai.dorayaki@gmail.com>
2017-12-09 13:37:14 +0900

    :sparkles: Make enable to render mesh grid

--- src/components/Map.js
+++ src/components/Map.js
@@ -17,7 +17,7 @@ import {
 import meshCalculator from '../domain/calculateMesh'
 import { round } from '../domain/roundPoint'
 
-import type { LatLng, Mesh } from '../domain/calculateMesh'
+import type { LatLng, Bounds, Mesh } from '../domain/calculateMesh'
 
 export type MapProps = {
   meshes: Array<Mesh>,
@@ -32,23 +32,29 @@ export type MapProps = {
 const initialLeafletBounds: Array<Array<number>> = [[35, 139], [37, 140]]
 const { latLngToMesh, SCALES } = meshCalculator
 
-const applyDatumToBounds = (bounds: Bounds, datum: string): LatLng => {
+/**
+ * Apply datum to bounds.
+ * 
+ * @param {Bounds} bounds bounds
+ * @param {string} datum datum(tokyo/wgs84)
+ * @returns {Bounds} bounds
+ */
+const applyDatumToBounds = (bounds: Bounds, datum: string): Bounds => {
   if (datum == 'Tokyo') {
     return convertBoundsToWGS84Datum(bounds)
   }
   return bounds
 }
 
-const calculateLeafletBoundsFrom = (
-  meshes: Array<Mesh>,
-  markerPositions: Array<LatLng>,
-  datum: string
-): Array<Array<number>> => {
-  if (meshes.length === 0 && markerPositions.length == 0) {
-    return initialLeafletBounds
-  }
-  let lats: Array<number> = []
-  let lngs: Array<number> = []
+/**
+ * Make the set of meshes a set of latitude and longitude.
+ * @param {Array<Mesh>} meshes 
+ * @param {string} datum 
+ * @returns {{ lats: Array<number>, lngs: Array<number> }} lats and lngs
+ */
+const meshesToLatsAndLngs = (meshes: Array<Mesh>, datum: string): { lats: Array<number>, lngs: Array<number> } => {
+  const lats: Array<number> = []
+  const lngs: Array<number> = []
   meshes
     .map(mesh => mesh.bounds)
     .map(bounds => applyDatumToBounds(bounds, datum))
@@ -58,6 +64,31 @@ const calculateLeafletBoundsFrom = (
       lats.push(latLng.lat)
       lngs.push(latLng.lng)
     })
+  return {
+    lats,
+    lngs
+  }
+}
+
+/**
+ * Calculate a LeafletBounds from mesh, the maker position and datum.
+ * 
+ * @param {Array<Mesh>} meshes 
+ * @param {Array<LatLng>} markerPositions 
+ * @param {string} datum
+ * @returns {Array<Array<number>>} LeafletBounds
+ */
+const calculateLeafletBoundsFrom = (
+  meshes: Array<Mesh>,
+  markerPositions: Array<LatLng>,
+  datum: string
+): Array<Array<number>> => {
+  if (meshes.length === 0 && markerPositions.length == 0) {
+    return initialLeafletBounds
+  }
+  const latsAndLngs = meshesToLatsAndLngs(meshes, datum)
+  const lats: Array<number> = latsAndLngs.lats
+  const lngs: Array<number> = latsAndLngs.lngs
 
   markerPositions.forEach(position => {
     lats.push(position.lat)
@@ -70,14 +101,26 @@ const calculateLeafletBoundsFrom = (
   ]
 }
 
+/**
+ * Create a card description from LatLng and datum.
+ * 
+ * @param {LatLng} latLng 
+ * @param {string} datum 
+ * @returns {string} card description
+ */
 const createCardDescription = (latLng: LatLng, datum: string): string => {
-  const convertedLatLng = convertLatLngToTokyoDatum(latLng, datum)
+  const convertedLatLng = datum == 'tokyo' ? convertLatLngToTokyoDatum(latLng) : latLng
   return `position: ${round(convertedLatLng.lat, 5)}, ${round(
     convertedLatLng.lng,
     5
   )}`
 }
 
+/**
+ * Create a card content from LatLng.
+ * @param {LatLng} param0 LatLng
+ * @returns card content
+ */
 const createCardContent = ({ lat, lng }: LatLng) =>
   SCALES.map((scale, idx) => (
     <Card.Content
@@ -86,7 +129,16 @@ const createCardContent = ({ lat, lng }: LatLng) =>
     />
   ))
 
-const getSquareMeshes = (redius: number): Array<Mesh> => {
+/**
+ * Get a square mesh from LatLng, zoom and redius.
+ * @param {LatLng} latlng 
+ * @param {number} zoom 
+ * @param {number} redius 
+ * @returns {Array<Mesh>} square meshes
+ */
+const getSquareMeshes = (latlng: LatLng, zoom: number, redius: number): Array<Mesh> => {
+  const scale: number = meshCalculator.getScaleWith(zoom)
+
   return ['5339', '5340', '5338', '5438', '5439', '5440', '5238', '5239', '5240'].map(code => {
     return {
       code,
@@ -95,6 +147,26 @@ const getSquareMeshes = (redius: number): Array<Mesh> => {
     }
   })
 }
+
+/**
+ * Create a mesh rectangle.
+ * @param {Bounds} bounds 
+ * @param {number} index 
+ * @param {string} meshCode 
+ * @param {string} color
+ * @returns {Rectangle} mesh rectangle
+ */
+const createMeshRect = (bounds: Bounds, index: number, meshCode: string, color: string = '#00847e'): Rectangle => (
+  <Rectangle
+    bounds={[bounds.leftTop, bounds.rightBottom]}
+    key={index}
+    color={color}
+  >
+    <Tooltip>
+      <span>{meshCode}</span>
+    </Tooltip>
+  </Rectangle>
+)
 
 const Map = (props: MapProps) => (
   <div style={{ width: '100%', height: '100%' }}>
@@ -115,34 +187,14 @@ const Map = (props: MapProps) => (
 
       {props.isShowDebugTiles && <DebugTileLayer />}
 
-      {getSquareMeshes(10).map((mesh, index) => {
-        const bounds = applyDatumToBounds(mesh.bounds, props.datum)
-        return (
-          <Rectangle
-            bounds={[bounds.leftTop, bounds.rightBottom]}
-            key={index}
-            color="#9C27B0"
-          >
-            <Tooltip>
-              <span>{mesh.code}</span>
-            </Tooltip>
-          </Rectangle>
-        )
+      {getSquareMeshes({ lat: 0, lng: 0 }, 6, 10).map((mesh, index) => {
+        const bounds: Bounds = applyDatumToBounds(mesh.bounds, props.datum)
+        return createMeshRect(bounds, index, mesh.code, '#9C27B0')
       })}
 
       {props.meshes.map((mesh, index) => {
         const bounds = applyDatumToBounds(mesh.bounds, props.datum)
-        return (
-          <Rectangle
-            bounds={[bounds.leftTop, bounds.rightBottom]}
-            key={index}
-            color="#00847e"
-          >
-            <Tooltip>
-              <span>{mesh.code}</span>
-            </Tooltip>
-          </Rectangle>
-        )
+        return createMeshRect(bounds, index, mesh.code)
       })}
 
       {props.markerPositions.map((position, idx) => (
