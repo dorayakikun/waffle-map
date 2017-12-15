e114282ca7e447e39334170ecb588da0ead96eee
dorayakikun <amakunai.dorayaki@gmail.com>
2017-12-16 01:45:12 +0900

    :art: Refactor Map.stories

--- stories/Map.stories.js
+++ stories/Map.stories.js
@@ -6,99 +6,40 @@ import Map from '../src/components/Map'
 
 import type { LatLng } from '../src/domain/calculateMesh'
 
-const propsNoMesh = {
+const someMesh = {
+  code: '5339',
+  center: { lat: 35.66667, lng: 139.5 },
+  bounds: {
+    leftTop: { lat: 36, lng: 139 },
+    rightBottom: { lat: 35.33334, lng: 140 }
+  }
+}
+const otherMesh = {
+  code: '5339',
+  center: { lat: 35.66667, lng: 139.5 },
+  bounds: {
+    leftTop: { lat: 36, lng: 139 },
+    rightBottom: { lat: 35.33334, lng: 140 }
+  }
+}
+
+const propsDefault = {
   meshes: [],
   contextmenuPosition: null,
   isShowDebugTiles: false,
+  isShowMeshes: false,
   markerPositions: [],
   datum: 'wgs84',
-  onContextmenu: (event: Event & { latlng: LatLng }) => {},
-  onClose: () => {}
+  onContextmenu: (event: Event & { latlng: LatLng }) => { },
+  onClose: () => { }
 }
-storiesOf('Map', module).add('no mesh', () => <Map {...propsNoMesh} />)
+const propsSomeMesh = Object.assign({}, { ...propsDefault, meshes: [someMesh] })
+const propsMeshes = Object.assign({}, { ...propsDefault, meshes: [someMesh, otherMesh] })
+const propsSomeMeshAndContextPosition = Object.assign({}, { ...propsSomeMesh, contextmenuPosition: { lat: 35.6896, lng: 139.6921 } })
+const propsShowDebugTile = Object.assign({}, { ...propsDefault, isShowDebugTiles: true })
 
-const propsSomeMesh = {
-  meshes: [
-    {
-      code: '5339',
-      center: { lat: 35.66667, lng: 139.5 },
-      bounds: {
-        leftTop: { lat: 36, lng: 139 },
-        rightBottom: { lat: 35.33334, lng: 140 }
-      }
-    }
-  ],
-  contextmenuPosition: null,
-  isShowDebugTiles: false,
-  markerPositions: [],
-  datum: 'wgs84',
-  onContextmenu: (event: Event & { latlng: LatLng }) => {},
-  onClose: () => {}
-}
+storiesOf('Map', module).add('no mesh', () => <Map {...propsDefault} />)
 storiesOf('Map', module).add('with mesh', () => <Map {...propsSomeMesh} />)
-
-const propsSomeMeshes = {
-  meshes: [
-    {
-      code: '5335',
-      center: { lat: 35.66667, lng: 135.5 },
-      bounds: {
-        leftTop: { lat: 36, lng: 135 },
-        rightBottom: { lat: 35.33334, lng: 136 }
-      }
-    },
-    {
-      code: '5339',
-      center: { lat: 35.66667, lng: 139.5 },
-      bounds: {
-        leftTop: { lat: 36, lng: 139 },
-        rightBottom: { lat: 35.33334, lng: 140 }
-      }
-    }
-  ],
-  contextmenuPosition: null,
-  isShowDebugTiles: false,
-  markerPositions: [],
-  datum: 'wgs84',
-  onContextmenu: (event: Event & { latlng: LatLng }) => {},
-  onClose: () => {}
-}
-
-storiesOf('Map', module).add('with meshes', () => <Map {...propsSomeMeshes} />)
-
-const propsSomeMeshAndContextPosition = {
-  meshes: [
-    {
-      code: '5339',
-      center: { lat: 35.66667, lng: 139.5 },
-      bounds: {
-        leftTop: { lat: 36, lng: 139 },
-        rightBottom: { lat: 35.33334, lng: 140 }
-      }
-    }
-  ],
-  contextmenuPosition: { lat: 35.6896, lng: 139.6921 },
-  isShowDebugTiles: false,
-  markerPositions: [],
-  datum: 'wgs84',
-  onContextmenu: (event: Event & { latlng: LatLng }) => {},
-  onClose: () => {}
-}
-
-storiesOf('Map', module).add('with meshes and contextMenuPosition', () => (
-  <Map {...propsSomeMeshAndContextPosition} />
-))
-
-const propsShowDebugTile = {
-  meshes: [],
-  contextmenuPosition: null,
-  isShowDebugTiles: true,
-  markerPositions: [],
-  datum: 'wgs84',
-  onContextmenu: (event: Event & { latlng: LatLng }) => {},
-  onClose: () => {}
-}
-
-storiesOf('Map', module).add('show Debug Tile', () => (
-  <Map {...propsShowDebugTile} />
-))
+storiesOf('Map', module).add('with meshes', () => <Map {...propsMeshes} />)
+storiesOf('Map', module).add('with meshes and contextMenuPosition', () => (<Map {...propsSomeMeshAndContextPosition} />))
+storiesOf('Map', module).add('show Debug Tile', () => (<Map {...propsShowDebugTile} />))
