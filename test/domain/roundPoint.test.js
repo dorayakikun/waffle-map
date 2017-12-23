ae6f6b6e73e187fa6b6c655f7b4e4231de69939a
dorayakikun <amakunai.dorayaki@gmail.com>
2017-12-23 17:15:20 +0900

    :shirt: Fix eslint issues

--- test/domain/roundPoint.test.js
+++ test/domain/roundPoint.test.js
@@ -1,15 +1,15 @@
 // @flow
 
-import test from 'ava'
-import { round } from '../../src/domain/roundPoint'
+import test from 'ava';
+import { round } from '../../src/domain/roundPoint';
 
 test('Should throw an error when place less than 1', t => {
   const error = t.throws(() => {
-    round(1.2345, 0)
-  })
+    round(1.2345, 0);
+  });
   t.is(
     error.message,
     `Invalid place found.
 The acceptable place is greater than 0.`
-  )
-})
+  );
+});
--- test/reducers/index.test.js
+++ test/reducers/index.test.js
@@ -1,184 +1,177 @@
 // @flow
 
-import test from 'ava'
-import * as AppActions from '../../src/actions/AppActions'
-import reducer from '../../src/reducers'
-import * as MeshCalculator from 'waffle-map-mesh-calculator-basic'
+import test from 'ava';
+import * as AppActions from '../../src/actions/AppActions';
+import reducer from '../../src/reducers';
+import * as MeshCalculator from 'waffle-map-mesh-calculator-basic';
 
 const defaultState = {
   markerInput: {
     latLng: '',
     unit: 'degree',
-    errorMessage: ''
+    errorMessage: '',
   },
   meshInput: {
     errorMessage: '',
     meshCodes: '',
     datum: 'WGS84',
-    separator: '.'
+    separator: '.',
   },
   tileToggle: {
-    isShowDebugTiles: false
+    isShowDebugTiles: false,
   },
   meshToggle: {
-    isShowMeshes: false
+    isShowMeshes: false,
   },
   meshes: [],
   map: {
     contextmenuPosition: null,
-    markerPositions: []
-  }
-}
+    markerPositions: [],
+  },
+};
 
 test('Should handle PUT_MARKER', t => {
-  const latLng = '35,139'
-  const unit = 'degree'
+  const latLng = '35,139';
+  const unit = 'degree';
   t.deepEqual(
     reducer(undefined, AppActions.putMarker(latLng, unit)),
-    Object.assign({}, {
+    {
       ...defaultState,
       markerInput: {
-        latLng: latLng, unit: unit, errorMessage: ''
+        latLng: latLng, unit: unit, errorMessage: '',
       },
       map: {
         contextmenuPosition: null,
-        markerPositions: [{ lat: 35, lng: 139 }]
-      }
-    }))
-})
+        markerPositions: [{ lat: 35, lng: 139 }],
+      },
+    });
+});
 
 test('Should handle PUT_MARKER when setting invalid latLng', t => {
-  const latLng = 'A,139'
-  const unit = 'degree'
+  const latLng = 'A,139';
+  const unit = 'degree';
   t.deepEqual(
     reducer(undefined, AppActions.putMarker(latLng, unit)),
-    Object.assign({}, {
+    {
       ...defaultState,
       markerInput: {
         latLng: latLng, unit: unit, errorMessage: `Unexpected lat found.
 Only numbers are acceptable.
-Actual: A,139`}
-    })
-  )
-})
+Actual: A,139`},
+    });
+});
 
 test('Should handle REMOVE_ALL_MARKERS', t => {
-  t.deepEqual(reducer(undefined, AppActions.removeAllMarkers()), defaultState)
-})
+  t.deepEqual(reducer(undefined, AppActions.removeAllMarkers()), defaultState);
+});
 
 test('Should handle INPUT_MESHES', t => {
-  const errorMessage = ''
-  const meshCodes = '5339'
+  const errorMessage = '';
+  const meshCodes = '5339';
   t.deepEqual(
     reducer(undefined, AppActions.inputMeshes(meshCodes)),
-    Object.assign({}, {
+    {
       ...defaultState,
       meshInput: {
         errorMessage,
         meshCodes,
         datum: 'WGS84',
-        separator: '.'
+        separator: '.',
       },
       meshes: [
         {
           code: meshCodes,
           center: MeshCalculator.meshToLatLng(meshCodes),
-          bounds: MeshCalculator.meshToBounds(meshCodes)
-        }
+          bounds: MeshCalculator.meshToBounds(meshCodes),
+        },
       ],
-    })
-  )
-})
+    });
+});
 
 test('Should handle INPUT_MESHES when setting invalid mesh code', t => {
   const errorMessage = `Invalid mesh code found.
 The length of the mesh code is 4, 6, or 8.
-The actual length is 3, the mesh code is 533.`
-  const meshCodes = '533'
+The actual length is 3, the mesh code is 533.`;
+  const meshCodes = '533';
   t.deepEqual(
     reducer(undefined, AppActions.inputMeshes(meshCodes)),
-    Object.assign({}, {
+    {
       ...defaultState,
       meshInput: {
         errorMessage,
         meshCodes,
         datum: 'WGS84',
-        separator: '.'
-      }
-    })
-  )
-})
+        separator: '.',
+      },
+    });
+});
 
 test('Should handle SELECT_DATUM', t => {
-  const datum = 'Tokyo'
-  t.deepEqual(reducer(undefined, AppActions.selectDatum(datum)), Object.assign({}, {
+  const datum = 'Tokyo';
+  t.deepEqual(reducer(undefined, AppActions.selectDatum(datum)), {
     ...defaultState, meshInput: {
       errorMessage: '',
       meshCodes: '',
       datum,
-      separator: '.'
-    }
-  }))
-})
+      separator: '.',
+    },
+  });
+});
 
 test('Should handle SELECT_SEPARATOR', t => {
-  const errorMessage = ''
-  const separator = ','
+  const errorMessage = '';
+  const separator = ',';
   t.deepEqual(
     reducer(undefined, AppActions.selectSeparator(separator)),
-    Object.assign({}, {
+    {
       ...defaultState,
       meshInput: {
         errorMessage,
         meshCodes: '',
         datum: 'WGS84',
-        separator
+        separator,
       },
-    })
-  )
-})
+    });
+});
 
 test('Should handle TOGGLE_DEBUG_TILES', t => {
-  const isShowDebugTiles = true
+  const isShowDebugTiles = true;
   t.deepEqual(
     reducer(undefined, AppActions.toggleDebugTiles(isShowDebugTiles)),
-    Object.assign({}, {
+    {
       ...defaultState,
       tileToggle: {
-        isShowDebugTiles
-      }
-    })
-  )
-})
+        isShowDebugTiles,
+      },
+    });
+});
 
 test('Should handle TOGGLE_MESHES', t => {
-  const isShowMeshes = true
+  const isShowMeshes = true;
   t.deepEqual(
     reducer(undefined, AppActions.toggleMeshes(isShowMeshes)),
-    Object.assign({}, {
+    {
       ...defaultState,
       meshToggle: {
         isShowMeshes,
-      }
-    })
-  )
-})
+      },
+    });
+});
 
 test('Should handle UPDATE_CONTEXTMENU_POSITION', t => {
-  const latLng = { lat: 35, lng: 139 }
+  const latLng = { lat: 35, lng: 139 };
   t.deepEqual(
     reducer(undefined, AppActions.updateContextmenuPosition(latLng)),
-    Object.assign({}, {
+    {
       ...defaultState,
       map: {
         contextmenuPosition: latLng,
-        markerPositions: []
-      }
-    })
-  )
-})
+        markerPositions: [],
+      },
+    });
+});
 
 test('Should return an initial state when setting an invalid action', t => {
-  const invalidAction = () => ({ type: 'INVALID_ACTION', payload: {} })
-  t.deepEqual(reducer(undefined, (invalidAction(): any)), defaultState)
-})
+  const invalidAction = () => ({ type: 'INVALID_ACTION', payload: {} });
+  t.deepEqual(reducer(undefined, (invalidAction(): any)), defaultState);
+});
