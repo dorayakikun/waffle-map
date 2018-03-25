6ec670ee90511bee8029d6895327ac58400184cf
Tomohide Takao <dorayaki.tomo@gmail.com>
2018-03-25 10:30:21 +0900

    refactor: Modify action keys type to an enum

--- src/actions/AppActions.ts
+++ src/actions/AppActions.ts
@@ -1,15 +1,5 @@
 import { LatLng } from '../domain/calculateMesh'
 
-export const PUT_MARKER = 'PUT_MARKER'
-export const REMOVE_ALL_MARKERS = 'REMOVE_ALL_MARKERS'
-export const CHANGE_UNIT = 'CHANGE_UNIT'
-export const INPUT_MESHES = 'INPUT_MESHES'
-export const SELECT_DATUM = 'SELECT_DATUM'
-export const SELECT_SEPARATOR = 'SELECT_SEPARATOR'
-export const TOGGLE_DEBUG_TILES = 'TOGGLE_DEBUG_TILES'
-export const TOGGLE_MESHES = 'TOGGLE_MESHES'
-export const UPDATE_CONTEXTMENU_POSITION = 'UPDATE_CONTEXTMENU_POSITION'
-
 export enum ActionKeys {
   PUT_MARKER = 'PUT_MARKER',
   REMOVE_ALL_MARKERS = 'REMOVE_ALL_MARKERS',
@@ -23,38 +13,38 @@ export enum ActionKeys {
 }
 
 interface PutMarkerAction {
-  readonly type: typeof PUT_MARKER,
+  readonly type: ActionKeys.PUT_MARKER,
   payload: { latLng: string },
 }
 interface RemoveAllMarkerAction {
-  readonly type: typeof REMOVE_ALL_MARKERS,
+  readonly type: ActionKeys.REMOVE_ALL_MARKERS,
 }
 interface ChangeUnitAction {
-  readonly type: typeof CHANGE_UNIT,
+  readonly type: ActionKeys.CHANGE_UNIT,
   payload: { unit: string },
 }
 interface InputMeshesAction {
-  readonly type: typeof INPUT_MESHES,
+  readonly type: ActionKeys.INPUT_MESHES,
   payload: { meshCodes: string },
 }
 interface SelectDatumAction {
-  readonly type: typeof SELECT_DATUM,
+  readonly type: ActionKeys.SELECT_DATUM,
   payload: { datum: string },
 }
 interface SelectSeparatorAction {
-  type: typeof SELECT_SEPARATOR,
+  type: ActionKeys.SELECT_SEPARATOR,
   payload: { separator: string },
 }
 interface ToggleDebugTilesAction {
-  readonly type: typeof TOGGLE_DEBUG_TILES,
+  readonly type: ActionKeys.TOGGLE_DEBUG_TILES,
   payload: { isShowDebugTiles: boolean },
 }
 interface ToggleMeshesAction {
-  type: typeof TOGGLE_MESHES,
+  type: ActionKeys.TOGGLE_MESHES,
   payload: { isShowMeshes: boolean },
 }
 interface UpdateContextmenuPositionAction {
-  type: typeof UPDATE_CONTEXTMENU_POSITION,
+  type: ActionKeys.UPDATE_CONTEXTMENU_POSITION,
   payload: { latLng?: LatLng },
 }
 
--- src/actions/__tests__/AppActions.test.ts
+++ src/actions/__tests__/AppActions.test.ts
@@ -1,34 +1,34 @@
-import * as AppActions from '../AppActions'
+import { ActionKeys, inputMeshes, selectSeparator, updateContextmenuPosition } from '../AppActions'
 
 test('Should create action to input meshes', () => {
   const meshCodes = '5339'
   const expectedAction = {
-    type: AppActions.INPUT_MESHES,
+    type: ActionKeys.INPUT_MESHES,
     payload: {
       meshCodes,
     },
   }
-  expect(AppActions.inputMeshes(meshCodes)).toEqual(expectedAction)
+  expect(inputMeshes(meshCodes)).toEqual(expectedAction)
 })
 
 test('Should create action to select separator', () => {
   const separator = '.'
   const expectedAction = {
-    type: AppActions.SELECT_SEPARATOR,
+    type: ActionKeys.SELECT_SEPARATOR,
     payload: {
       separator,
     },
   }
-  expect(AppActions.selectSeparator(separator)).toEqual(expectedAction)
+  expect(selectSeparator(separator)).toEqual(expectedAction)
 })
 
 test('Should create action to update contextmenu position', () => {
   const latLng = { lat: 35, lng: 139 }
   const expectedAction = {
-    type: AppActions.UPDATE_CONTEXTMENU_POSITION,
+    type: ActionKeys.UPDATE_CONTEXTMENU_POSITION,
     payload: {
       latLng,
     },
   }
-  expect(AppActions.updateContextmenuPosition(latLng)).toEqual(expectedAction)
+  expect(updateContextmenuPosition(latLng)).toEqual(expectedAction)
 })
