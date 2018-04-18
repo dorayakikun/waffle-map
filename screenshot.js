81f6d5edd112997c7a80a0a6f8a10c78e63789e7
Tomohide Takao <dorayaki.tomo@gmail.com>
2018-04-18 22:25:40 +0900

    chore: Update regression testing

--- screenshot.js
+++ screenshot.js
@@ -1,23 +1,36 @@
 const puppeteer = require('puppeteer')
+const connect = require('connect')
+const serveStatic = require('serve-static')
+const del = require('del')
 const fs = require('fs')
-const config = JSON.parse(fs.readFileSync('./screenshot.config.json', 'utf8'));
+const config = JSON.parse(fs.readFileSync('./screenshot.config.json', 'utf8'))
 
-(async () => {
+del.sync([config.outdir])
+fs.mkdirSync(config.outdir)
+
+const app = connect()
+app.use(serveStatic(config.storybookdir))
+server = app.listen(6006)
+;(async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-
-  for (const kind of Object.keys(config.storiesByKind)) {
-    for (const story of config.storiesByKind[kind]) {
-      const storyName = encodeURIComponent(story.name)
-      await page.goto(
-        `http://127.0.0.1:6006/iframe.html?selectedKind=${kind}&selectedStory=${storyName}`,
-        { waitUntil: 'networkidle2' }
-      )
-      await page.screenshot({
-        path: `./__screenshots__/${kind}-${story.name}.png`,
-      })
+  try {
+    for (const kind of Object.keys(config.storiesByKind)) {
+      for (const story of config.storiesByKind[kind]) {
+        const storyName = encodeURIComponent(story.name)
+        await page.goto(
+          `http://127.0.0.1:6006/iframe.html?selectedKind=${kind}&selectedStory=${storyName}`,
+          { waitUntil: 'networkidle2' }
+        )
+        await page.screenshot({
+          path: `./__screenshots__/${kind}-${story.name}.png`,
+        })
+      }
     }
+  } catch (e) {
+    console.log(e)
+  } finally {
+    await browser.close()
+    server.close()
   }
-
-  await browser.close()
 })()
