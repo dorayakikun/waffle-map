a2d5fef1552054a4ddec12feb987d312673ba826
Tomohide Takao <amakunai.dorayaki@gmail.com>
2018-05-19 14:16:00 +0900

    fix: config.yml

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -2,7 +2,7 @@ const siteConfig = {
   title: 'Waffle Map' /* title for your website */,
   tagline: 'Fantastic mesh renderer',
   url: 'https://dorayakikun.github.io/waffle-map/' /* your website url */,
-  baseUrl: '/' /* base url for your project */,
+  baseUrl: '/waffle-map/' /* base url for your project */,
   projectName: 'waffle-map',
   organizationName: 'dorayakikun',
   headerLinks: [
@@ -30,9 +30,7 @@ const siteConfig = {
   onPageNav: 'separate',
   ogImage: 'img/favicon.png',
   twitterImage: 'img/favicon.png',
-  repoUrl: 'https://github.com/dorayakikun/waffle-map',
-  CIRCLE_PROJECT_USERNAME: 'dorayakikun',
-  CIRCLE_PROJECT_REPONAME: 'waffle-map'
+  repoUrl: 'https://github.com/dorayakikun/waffle-map'
 };
 
 module.exports = siteConfig;
