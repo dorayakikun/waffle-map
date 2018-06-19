29ec621a55e4490b14f1be93317b7f752b047ae0
Tomohide Takao <amakunai.dorayaki@gmail.com>
2018-06-19 19:04:48 +0900

    refactor: Apply redux-saga

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, Store } from 'redux'
+import { applyMiddleware, createStore, Store } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 import { Action } from './actions/AppActions'
 import { AppContainer } from './containers/App'
 import { reducers, State } from './reducers'
-
-const store: Store<State> = createStore(reducers)
+const sagaMiddleware = createSagaMiddleware()
+const store: Store<State> = createStore(reducers, applyMiddleware(sagaMiddleware))
 const root = document.getElementById('root')
 render(
   <Provider store={store}>
