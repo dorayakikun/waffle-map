75fd88acbeaaa66622ecf03c01e68a62820e717f
Tomohide Takao <amakunai.dorayaki@gmail.com>
2018-06-27 23:50:40 +0900

    refactor: Rollback to webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,20 @@
 const path = require('path')
 const webpack = require('webpack')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 const BASE_PLUGINS = [
   new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
     'process.env.npm_package_wafflemap_meshcalculator': JSON.stringify(
       process.env.npm_package_wafflemap_meshcalculator
     ),
   }),
 ]
 
-console.log('env >', process.env.NODE_ENV)
 module.exports = {
   entry: './src/index.tsx',
+  devServer: {
+    contentBase: 'public/',
+    historyApiFallback: true,
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
