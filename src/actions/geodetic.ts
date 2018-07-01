d277cb051f17da1418830d703c65de29ca348a35
Tomohide Takao <amakunai.dorayaki@gmail.com>
2018-07-01 16:46:18 +0900

    refactor: Add geodetic action/reducer

---
+++ src/actions/geodetic.ts
@@ -0,0 +1,27 @@
+export enum ActionKeys {
+  CHANGE_DATUM = 'geodetic/change_datum',
+  CHANGE_UNIT = 'geodetic/change_unit',
+}
+
+interface ChangeDatumAction {
+  readonly type: ActionKeys.CHANGE_DATUM
+  payload: { datum: string }
+}
+interface ChangeUnitAction {
+  readonly type: ActionKeys.CHANGE_UNIT
+  payload: { unit: string }
+}
+
+export type Action =
+  | ChangeDatumAction
+  | ChangeUnitAction
+
+export const changeDatum = (datum: string): ChangeDatumAction => ({
+  payload: { datum },
+  type: ActionKeys.CHANGE_DATUM,
+})
+
+export const changeUnit = (unit: string): ChangeUnitAction => ({
+  payload: { unit },
+  type: ActionKeys.CHANGE_UNIT,
+})
