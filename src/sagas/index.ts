651d7f1fd12d8641decb899974f773583540edf0
Tomohide Takao <amakunai.dorayaki@gmail.com>
2018-07-02 21:48:15 +0900

    fix: Fix sagas

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -7,21 +7,66 @@ import {
   take,
   takeEvery,
 } from 'redux-saga/effects'
-import * as Actions from '../actions/AppActions'
+import { removeAllMarkers as _removeAllMarkers } from '../actions/map'
+import {
+  ActionKeys as MarkerActionKeys,
+  changeErrorMessage as changeMarkersErrorMessage,
+  inputLatLng,
+  PutMarkerAction,
+} from '../actions/marker'
+import {
+  ActionKeys as MeshCodesActionKeys,
+  changeErrorMessage as changeMeshCodesErrorMessage,
+} from '../actions/meshCodes'
+import {
+  ActionKeys as MeshesActionKeys,
+  changeMeshes
+} from '../actions/meshes'
 import { getMeshCodesInput } from '../reducers'
+import { mapToMeshes } from '../reducers/meshes'
+
 
 function* createMeshes() {
-  const { meshCodes, separator } = yield select(getMeshCodesInput)
-  yield put(Actions.createMeshes(meshCodes, separator))
+  const { meshCodes, separator } = yield select(getMeshCodesInput) // TODO fix to getMeshCodes
+  let errorMessage = ''
+  try {
+    const meshes = mapToMeshes(meshCodes, separator)
+    console.log('meshes: %o', meshes)
+    yield put(changeMeshes(meshes))
+    // TODO 基本的にErrorを含む処理はreducerに含めないほうが良さそう
+  } catch (e) {
+    errorMessage = e.message
+  }
+  yield put(changeMeshCodesErrorMessage(errorMessage))
 }
 
 function* inputMeshCodes() {
   while (true) {
-    yield take(Actions.ActionKeys.INPUT_MESH_CODES)
+    yield take(MeshCodesActionKeys.INPUT_MESH_CODES)
     yield call(createMeshes)
   }
 }
 
+function* putMarker(action: PutMarkerAction) {
+  yield put(inputLatLng(action.payload.latLng))
+  // TODO 関連ファイルをmarkersにリネーム あと meshCodesも
+  let errorMessage = ''
+  try {
+    // yield put(concatMarkerPositions)
+  } catch (e) {
+    errorMessage = e.message
+  }
+  yield put(changeMarkersErrorMessage(errorMessage))
+}
+
+function* removeAllMarkers() {
+  yield put(_removeAllMarkers())
+}
+
 export function* rootSaga() {
-  yield all([fork(inputMeshCodes)])
+  yield all([
+    fork(inputMeshCodes),
+    takeEvery(MarkerActionKeys.PUT_MARKER, putMarker),
+    takeEvery(MarkerActionKeys.REMOVE_ALL_MARKERS, removeAllMarkers),
+  ])
 }
