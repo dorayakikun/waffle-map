ba08cd4bdf64baf4b8cc24c62009cc30fdbe946f
Tomohide Takao <amakunai.dorayaki@gmail.com>
2018-07-05 21:01:37 +0900

    chore: Add redux-devtools-extension

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,22 @@
 import * as React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { applyMiddleware, createStore, Store } from 'redux'
+import { applyMiddleware, compose, createStore } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import { AppContainer } from './containers/App'
 import { reducers } from './reducers'
 import { rootSaga } from './sagas'
 
 const sagaMiddleware = createSagaMiddleware()
-const store = createStore(reducers, applyMiddleware(sagaMiddleware))
+const composeEnhancers =
+  typeof window === 'object' &&
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    }) : compose
+const enhancer = composeEnhancers(
+  applyMiddleware(sagaMiddleware),
+)
+const store = createStore(reducers, enhancer)
 sagaMiddleware.run(rootSaga)
 
 const root = document.getElementById('root')
