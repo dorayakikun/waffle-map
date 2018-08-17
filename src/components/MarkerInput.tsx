b19c7a06277cdf92b77956425d21d590dff4cb00
Tomohide Takao <amakunai.dorayaki@gmail.com>
2018-08-17 20:58:41 +0900

    chore: Add test cases to MarkerInput.test.tsx

--- src/components/MarkerInput.tsx
+++ src/components/MarkerInput.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react'
 import {
   Button,
-  Dropdown,
-  DropdownProps,
   Input,
   InputOnChangeData,
   Message,
---
+++ src/components/__tests__/MarkerInput.test.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import * as render from 'react-test-renderer'
+import { MarkerInput, Props, State } from '../MarkerInput'
+
+const props: Props = {
+  errorMessage: '',
+  latLng: '',
+  putMarker: (event: React.SyntheticEvent<HTMLElement>, state: State) => {},
+  removeAllMarkers: () => {},
+}
+
+test('Should set props to MarkerInput', () => {
+  const tree = render.create(<MarkerInput {...props} />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+test('Should set props to MarkerInput(datum is Tokyo)', () => {
+  const errorMessage = 'It seems there was something wrong ...'
+  const latLng = 'lat,lng'
+  const newProps = { ...props, errorMessage, latLng }
+  const tree = render.create(<MarkerInput {...newProps} />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
