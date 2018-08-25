e71c6ed8887107f7ca4e3291a085b5e8c14cdaa1
Tomohide Takao <amakunai.dorayaki@gmail.com>
2018-08-25 20:49:14 +0900

    refactor: Improve the appearance(DebugTileLayer)

--- src/components/DebugTileLayer.tsx
+++ src/components/DebugTileLayer.tsx
@@ -7,14 +7,17 @@ const DebugTileLayer = withLeaflet(
       const debugGridLayer = (Leaflet.GridLayer as any).extend({
         createTile: (coords: Leaflet.Coords): HTMLElement => {
           const tile = document.createElement('div')
-          tile.style.outline = '1px solid red'
-          tile.innerHTML = `<span style="font-family: Lato, 
-                                       'Helvetica Neue', 
-                                       Helvetica, 
-                                       Arial, 
-                                       sans-serif; 
-                                       font-size: 24px; 
-                                       font-weight: 700;">
+          tile.style.backgroundColor = 'rgba(41, 98, 255, 0.2)'
+          tile.style.outline = '2px solid'
+          tile.style.outlineColor = '#2962FF'
+          tile.innerHTML = `<span style="
+                                      font-family: Lato, 
+                                      'Helvetica Neue', 
+                                      Helvetica, 
+                                      Arial, 
+                                      sans-serif; 
+                                      font-size: 24px; 
+                                      font-weight: 700;">
           ${[coords.x, coords.y, coords.z].join(', ')}
           </span>`
           return tile
