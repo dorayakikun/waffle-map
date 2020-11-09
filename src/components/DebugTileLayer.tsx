f2afb06fd9c6a7b2061c11dcc95ea43d2713e486
Tomohide Takao <github@dorayakikun.com>
2020-11-09 09:49:40 +0900

    fix: Migrate down react-leaflet to 2.8.x (#422)

--- src/components/DebugTileLayer.tsx
+++ src/components/DebugTileLayer.tsx
@@ -1,28 +1,30 @@
 import * as Leaflet from "leaflet";
-import { createTileLayerComponent } from "@react-leaflet/core";
+import { GridLayer, withLeaflet } from "react-leaflet";
 
-const d = (Leaflet.GridLayer as any).extend({
-  createTile: (coords: Leaflet.Coords): HTMLElement => {
-    const tile = document.createElement("div");
-    tile.style.backgroundColor = "rgba(41, 98, 255, 0.2)";
-    tile.style.outline = "2px solid";
-    tile.style.outlineColor = "#2962FF";
-    tile.innerHTML = `<span style="
-                                font-family: Lato, 
-                                'Helvetica Neue', 
-                                Helvetica, 
-                                Arial, 
-                                sans-serif; 
-                                font-size: 24px; 
-                                font-weight: 700;">
-    ${[coords.x, coords.y, coords.z].join(", ")}
-    </span>`;
-    return tile;
-  },
-});
-
-// eslint-disable-next-line
-const DebugTileLayer = createTileLayerComponent(function createTileLayer({}, context) {
-  return { instance: new d(), context };
-});
+const DebugTileLayer = withLeaflet(
+  class extends GridLayer {
+    public createLeafletElement(props: any): Leaflet.GridLayer {
+      const DebugGridLayer = (Leaflet.GridLayer as any).extend({
+        createTile: (coords: Leaflet.Coords): HTMLElement => {
+          const tile = document.createElement("div");
+          tile.style.backgroundColor = "rgba(41, 98, 255, 0.2)";
+          tile.style.outline = "2px solid";
+          tile.style.outlineColor = "#2962FF";
+          tile.innerHTML = `<span style="
+                                      font-family: Lato, 
+                                      'Helvetica Neue', 
+                                      Helvetica, 
+                                      Arial, 
+                                      sans-serif; 
+                                      font-size: 24px; 
+                                      font-weight: 700;">
+          ${[coords.x, coords.y, coords.z].join(", ")}
+          </span>`;
+          return tile;
+        },
+      });
+      return new DebugGridLayer(this.getOptions(props));
+    }
+  }
+);
 export { DebugTileLayer };
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import {
-  MapContainer as LeafletMap,
+  Map as LeafletMap,
   Marker,
   Popup,
   Rectangle,
